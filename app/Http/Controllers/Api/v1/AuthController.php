c11eeab38c57d6845005828b31c4f2b95ccc016f
Harrison-Muraya <harrisonmuraya8@gmail.com>
2026-01-03 02:39:38 +0300

    Added authentication API endpoints and updated User model to include Sanctum token support

--- app/Http/Controllers/Api/v1/AuthController.php
+++ app/Http/Controllers/Api/v1/AuthController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers\Api\v1;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUserRequest;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class AuthController extends Controller
 {
@@ -12,9 +15,19 @@ class AuthController extends Controller
         //
     }
 
-    public function register(Request $request)
+    public function register(StoreUserRequest $request): JsonResponse
     {
-        //
+        $user = User::create($request->validated());
+        $access_token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'success' => true,
+            'data' => [
+                'user' => $user,
+                'access_token' => $access_token,
+                'token_type' => 'Bearer',
+            ],
+        ], 201);
     }
 
     public function logout(Request $request)
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\v1\AuthController;
 
 
 
+Route::prefix('v1')->group(function () {
+    Route::prefix('auth')->group(function (){
+        Route::post('/login',[AuthController::class,'login']);
+        Route::post('/register',[AuthController::class,'register']);
+    });
+   
+});
