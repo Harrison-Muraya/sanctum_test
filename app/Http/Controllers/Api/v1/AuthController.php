b0655721b3e6108c0325964637037083969854fe
Harrison-Muraya <harrisonmuraya8@gmail.com>
2026-01-03 01:04:29 +0300

    Added Laravel Sanctum and API routing configuration

---
+++ app/Http/Controllers/Api/v1/AuthController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        //
+    }
+
+    public function register(Request $request)
+    {
+        //
+    }
+
+    public function logout(Request $request)
+    {
+        //
+    }
+
+    public function user(Request $request)
+    {
+        //
+    }
+
+}
